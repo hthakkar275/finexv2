1f24f35c24be6b9cd9a455e32b955a01f0effa0b
Hemant.Thakkar <hthakkar275@gmail.com>
2020-11-15 14:00:25 -0600

    Add distributed tracing with zipkin, kafka and cassandra

--- financial-exchange-env/finex-teardown.sh
+++ financial-exchange-env/finex-teardown.sh
@@ -1,30 +1,111 @@
 #!/bin/bash
 
-# Delete all but the finex-application-services and finex-internal-alb stacks
-# These two stacks are dependencies for other stacks whereby output exported by
-# these two stacks are cross-stack-referened. So we need to wait a while before 
-# the "child" stacks are well underway for deletion. The instance id values
-# exported by finex-application-services is used by the finex-internal-alb-listener
-# and finex-external-alb-listener for creating target groups. The ARN of ALB
-# created by finex-internal-alb and finex-external-alb are used by their 
-# respective listnner stacks.
+function showUsage() {
+    echo "    " >&2
+    echo "$(basename $0) -b delete-target "
+    echo "    " >&2
+    echo "               -b delete-target  required argument   " >&2
+    echo "                               business | middleware | infra | all " >&2
+    echo "                               business - delete application, api gateway, discover and config services   " >&2
+    echo "                               middleware - delete cassandra, kafka, zookeeper and zipking services" >&2
+    echo "                               infra - delete load balancers and nat gateways " >&2
+    echo "                               all - delete business, middleware and infra  " >&2
+    echo "   " >&2
+}
 
-for STACK in "finex-dns-us-east-2" \
-"finex-application-services-us-east-2-zone1" \
-"finex-apigateway-service-us-east-2-zone1" \
-"finex-discovery-service-us-east-2-zone1" \
-"finex-config-service-us-east-2-zone1"; do
-    aws cloudformation delete-stack --stack-name $STACK   
+function delete_business_services_stacks() {
+    for STACK in "finex-dns-us-east-2" \
+    "finex-application-services-us-east-2-zone1" \
+    "finex-apigateway-service-us-east-2-zone1" \
+    "finex-discovery-service-us-east-2-zone1" \
+    "finex-config-service-us-east-2-zone1" \
+    "finex-zipkin-services-us-east-2-zone1"; do
+       aws cloudformation delete-stack --stack-name $STACK   
+    done
+
+    for i in {1..10}; do
+        echo "Deleting application, api gateway, discover and config services stacks ..."
+        sleep 30
+    done
+    echo "Exiting delete_business_services_stacks"
+}
+
+function delete_middleware_services_stacks() {
+
+    for MSTACK in "finex-kafka-services-us-east-2" \
+    "finex-cassandra-services-us-east-2"; do
+        echo "Deleting $MSTACK..."
+        aws cloudformation delete-stack --stack-name $MSTACK   
+    done
+
+    for i in {1..10}; do
+        echo "Deleting kafka and cassandra stacks ..."
+        sleep 30
+    done
+    echo "Exiting delete_middleware_services_stacks"
+
+}
+
+function delete_infra_services_stacks() {
+
+    for ESTACK in "finex-nat-gateway-us-east-2-zone1" \
+    "finex-nat-gateway-us-east-2-zone2" \
+    "finex-external-alb-us-east-2" \
+    "finex-internal-alb-us-east-2"; do
+        aws cloudformation delete-stack --stack-name $ESTACK
+    done
+
+    for i in {1..20}; do
+        echo "Deleting load balancers and nat gateway stacks ..."
+        sleep 30
+    done
+    echo "Exiting delete_infra_services_stacks"
+
+}
+
+function delete_all() {
+    delete_business_services_stacks
+    delete_middleware_services_stacks
+    delete_infra_services_stacks
+}
+
+while getopts "b:h" opt; do
+  case ${opt} in
+    b )
+      FINEX_DELETE_TARGET=$OPTARG
+      ;;
+    h )
+      showUsage
+      exit 0
+      ;;
+    \? )
+      echo "****** Invalid argument $opt"
+      showUsage
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;  
+  esac
 done
 
-# Wait 30 seconds for the dependent stacks to be well underway in the deletion
-# before proceeding to issue delete stack commands on their dependencies
-sleep 600
+if [[ -z "$FINEX_DELETE_TARGET" ]]; then
+    echo "Missing delete target. Must be one of [business, middleware, infra, all]"
+    exit 1
+fi
 
-echo "Deleting load balancer stacks"
+if [[ "$FINEX_DELETE_TARGET" == "business" ]]; then
+    delete_business_services_stacks
+fi 
 
-for ESTACK in "finex-nat-gateway-us-east-2" \
-"finex-external-alb-us-east-2" \
-"finex-internal-alb-us-east-2"; do
-    aws cloudformation delete-stack --stack-name $ESTACK
-done
+if [[ "$FINEX_DELETE_TARGET" == "middleware" ]]; then
+    delete_middleware_services_stacks
+fi 
+
+if [[ "$FINEX_DELETE_TARGET" == "infra" ]]; then
+    delete_infra_services_stacks
+fi 
+
+if [[ "$FINEX_DELETE_TARGET" == "all" ]]; then
+    delete_all
+fi 
